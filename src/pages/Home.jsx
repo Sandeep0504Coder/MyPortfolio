fc71dba3819a66f144dbe78f3be6668896fa4cb1
Sandeep Mandal <sandeepmandal0504@gmail.com>
2025-01-30 23:49:34 +0530

    Code modification

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,9 +6,11 @@ import About from "./miniComponents/About";
 import Portfolio from "./miniComponents/Portfolio";
 import Contact from "./miniComponents/Contact";
 import axios from "axios";
+import Loader from "../components/Loader";
 const server = import.meta.env.VITE_SERVER;
 
 const Home = () => {
+  const [loading, setLoading] = useState(true);
   const [user, setUser] = useState({
       fullName: "",
       objective: "",
@@ -25,6 +27,7 @@ const Home = () => {
 
     const getMyProfile = async () => {
       try{
+        setLoading(true);
         const { data } = await axios.get(
           `${server}/api/v1/user/portfolio/me`,
           { withCredentials: true }
@@ -33,6 +36,8 @@ const Home = () => {
         setUser(data.user);  
       } catch( e ){
         console.log( e );
+      } finally {
+        setLoading(false); // Stop loading
       }
     };
     useEffect(() => {
@@ -40,12 +45,12 @@ const Home = () => {
     }, []);
   return (
     <article className="px-5 mt-10 sm:mt-14 md:mt-16 lg:mt-24 xl:mt-32 sm:mx-auto w-full max-w-[1050px] flex flex-col gap-14">
-      <Hero user = {user}/>
+      { loading ? <Loader/> : <><Hero user = {user}/>
       <Timeline />
       <About user = {user}/>
       <Skills />
       <Portfolio />
-      <Contact />
+      <Contact /></> }
     </article>
   );
 };
